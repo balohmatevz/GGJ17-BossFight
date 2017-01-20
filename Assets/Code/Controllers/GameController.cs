8b0dfa90ba4739b3095f4ab36c6757b1e3f4b46a
Liam <contact@liamlime.com>
2017-01-20 22:52:28 +0100

    - Adds monster framework and camera movement

---
+++ Assets/Code/Controllers/GameController.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public static GameController obj;
+
+    #region CONSTANTS
+
+    public const int MONSTER_HEALTH = 100;
+
+    #endregion CONSTANTS
+
+    #region PUBLIC MEMBERS
+
+    [Header("Variables")]
+    public int health;
+    public Vector2 LastMousePos;
+
+    [Header("Scene references")]
+    public Camera cam;
+    public Transform camT;
+
+    #endregion PUBLIC MEMBERS
+
+    #region UNITY METHODS
+    // Use this for initialization
+    private void Awake()
+    {
+        Bootstrap();
+    }
+
+    void Start()
+    {
+        Init();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Frame();
+    }
+    #endregion UNITY METHODS
+
+    public void Bootstrap()
+    {
+        obj = this;
+    }
+
+    public void Init()
+    {
+        health = MONSTER_HEALTH;
+        LastMousePos = Input.mousePosition;
+    }
+
+    public void Frame()
+    {
+        Vector2 deltaMousePos = (Vector2)Input.mousePosition - (Vector2)LastMousePos;
+
+        camT.Rotate(0, deltaMousePos.x * 0.4f, 0, Space.World);
+        camT.Rotate(-deltaMousePos.y * 0.4f, 0, 0, Space.Self);
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            FireShot(camT.position, camT.forward);
+        }
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            //UP
+            camT.Translate(0, 10 * Time.deltaTime, 0, Space.Self);
+        }
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            //DOWN
+            camT.Translate(0, -10 * Time.deltaTime, 0, Space.Self);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            //FORWARD
+            camT.Translate(0, 0, 10 * Time.deltaTime, Space.Self);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            //BACK
+            camT.Translate(0, 0, -10 * Time.deltaTime, Space.Self);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            //LEFT
+            camT.Translate(-10 * Time.deltaTime, 0, 0, Space.Self);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            //RIGHT
+            camT.Translate(10 * Time.deltaTime, 0, 0, Space.Self);
+        }
+
+        LastMousePos = Input.mousePosition;
+    }
+
+    //Fires a shot from origin in the spuuplied direction
+    public void FireShot(Vector3 origin, Vector3 direction)
+    {
+        Ray ray = new Ray(origin, direction);
+        RaycastHit hit = new RaycastHit();
+        Physics.Raycast(ray, out hit);
+        if (hit.collider && hit.collider.gameObject)
+        {
+            MonsterPart monsterPart = hit.collider.gameObject.GetComponent<MonsterPart>();
+            if (monsterPart != null)
+            {
+                if (monsterPart.CanBeHit)
+                {
+                    monsterPart.OnHit();
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Code/Monaster/Parts/MonsterPart.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterPart : MonoBehaviour
+{
+    public const float BLINK_SPEED = 0.2f;
+    public static Color BLINK_COLOR = Color.red;
+    public const float BLINK_DURATION = 2f;
+
+    public bool CanBeHit = true;
+    public Material mat;
+
+    public Color OriginalColor = Color.white;
+    public bool IsBlinkColorChanged = false;
+    public float HitTimer = 0f;
+    public float BlinkTimer = 0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        mat = this.GetComponent<Renderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (HitTimer > 0)
+        {
+            HitTimer -= Time.deltaTime;
+            BlinkTimer -= Time.deltaTime;
+
+            if (BlinkTimer <= 0)
+            {
+                //Swap blink color
+                BlinkTimer = BLINK_SPEED;
+                if (IsBlinkColorChanged)
+                {
+                    //Blinking from original to red
+                    IsBlinkColorChanged = false;
+                    mat.color = BLINK_COLOR;
+                }
+                else
+                {
+                    //Blinking from red to original
+                    IsBlinkColorChanged = true;
+                    mat.color = OriginalColor;
+                }
+            }
+
+            if (HitTimer <= 0)
+            {
+                //Blinking over
+                mat.color = OriginalColor;
+            }
+        }
+    }
+
+    public void OnHit()
+    {
+        if (HitTimer > 0)
+        {
+            //Already blinking
+            return;
+        }
+
+        OriginalColor = mat.color;
+        HitTimer = BLINK_DURATION;
+        IsBlinkColorChanged = true;
+        BlinkTimer = BLINK_SPEED;
+        mat.color = BLINK_COLOR;
+    }
+}
