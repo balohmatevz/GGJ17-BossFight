a2d5de6b010ac16a5ee1b71e290d2c0d0e1906e1
Jernej Petaros <jernej.petaros@razum.si>
2017-01-21 10:44:44 +0100

    Created a FollowTarget script for the camera

---
+++ Assets/Scripts/FollowTarget.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTarget : MonoBehaviour
+{
+    public Transform Target;
+
+    public float DesiredDistance = 15f;
+    //public float Smoothing = 0.6f;
+
+    void Awake()
+    {
+            
+    }
+    
+    // Update is called once per frame
+    void Update ()
+    {
+        float newZ;
+        float desiredZ = newZ = Target.position.z - DesiredDistance;
+        //float newZ = Mathf.Lerp(transform.position.z, desiredZ, Smoothing);
+        transform.position = new Vector3(Target.position.x, transform.position.y, newZ);
+    }
+}
