13a30c17226699f1d153d03c4848d0f20af8a1df
Liam <contact@liamlime.com>
2017-01-21 11:23:59 +0100

    - Adds bullet fire and collision particle effects http://l1m.eu/nkplpybe. Bullets will only collide with what's in the BulletsWillCollide layer.

--- Assets/Code/Monaster/Parts/Bullet.cs
+++ Assets/Code/Monaster/Parts/Bullet.cs
@@ -4,35 +4,68 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    public const float HOLE_POSITION_DIFFERENCE = 0.3f;
+
     public Vector3 origin;
     public float maxDist;
+    public float Speed;
+    public bool IsDisabled = true;
 
     public Transform t;
     public Rigidbody rb;
+
+    public ParticleSystem Death;
+    public ParticleSystem Hole;
+    public GameObject DeathGO;
+    public GameObject HoleGO;
 
     // Use this for initialization
     void Start()
     {
         t = this.transform;
+        Death.Stop();
+        Hole.Stop();
     }
 
     // Update is called once per frame
     void Update()
     {
+        this.transform.Translate(0, 0, Speed * Time.deltaTime, Space.Self);
         if (Vector3.Distance(this.transform.position, origin) >= maxDist)
         {
             Destroy(this.gameObject);
         }
     }
 
-    public void SetUp(Vector3 origin, float rotation, float force, float maxDist)
+    public void SetUp(Vector3 origin, float rotation, float speed, float maxDist)
     {
         t = this.transform;
         rb = this.GetComponent<Rigidbody>();
         t.position = origin;
         t.rotation = Quaternion.Euler(0, rotation, 0);
-        rb.AddForce(t.forward * force);
+        this.Speed = speed;
         this.origin = origin;
         this.maxDist = maxDist;
+        IsDisabled = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!IsDisabled)
+        {
+            OnHit();
+        }
+    }
+
+    public void OnHit()
+    {
+        if (!IsDisabled)
+        {
+            Death.transform.Translate(0, 0, -HOLE_POSITION_DIFFERENCE, Space.Self);
+            Hole.transform.Translate(0, 0, -HOLE_POSITION_DIFFERENCE, Space.Self);
+            Death.Emit(1);
+            Hole.Emit(1);
+            IsDisabled = true;
+        }
     }
 }
