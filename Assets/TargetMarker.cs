629283c99589d7ff401982cedcdd129886991f88
Liam <contact@liamlime.com>
2017-01-21 01:19:01 +0100

    - Adds rocket projectiles peew peew

---
+++ Assets/TargetMarker.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetMarker : MonoBehaviour
+{
+    public GameObject Target;
+    public const float Y_POSITION = 0.05f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Target == null)
+        {
+            return;
+        }
+
+        if (GameController.obj.RocketsInFlight != 0)
+        {
+            //A rocket is in flight, keep target where it is.
+            return;
+        }
+
+        Vector3 pos = Target.transform.position;
+        pos.y = Y_POSITION;
+        this.transform.localPosition = pos;
+    }
+}
