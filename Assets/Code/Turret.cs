8ec08f0f468db2ab7a74836eb0d2d1357766779f
Jernej Petaros <jernej.petaros@razum.si>
2017-01-21 17:12:21 +0100

    Fixed a bug where OnDeath has been called multiple times Added logic to disable colliders on Turrets 5 seconds after OnDeath trigger

--- Assets/Code/Turret.cs
+++ Assets/Code/Turret.cs
@@ -6,10 +6,13 @@ public class Turret : MonoBehaviour
 {
     public Animator TurretAnimator;
 
+    private bool isDead = false;
+    private Collider[] colliders;
+
     // Use this for initialization
     void Start()
     {
-
+        colliders = GetComponentsInChildren<Collider>();
     }
 
     // Update is called once per frame
@@ -21,5 +24,20 @@ public class Turret : MonoBehaviour
     public void OnDeath()
     {
         TurretAnimator.SetBool("Destroyed", true);
+
+        if (!isDead)
+        {
+            isDead = true;
+            Invoke("DisableColliders", 5f);
+        }
+    }
+
+    public void DisableColliders()
+    {
+        Debug.Log("Disabling colliders.");
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            colliders[i].enabled = false;
+        }
     }
 }
